3c6f49e8bc1a0dfddd4b091f09d674be4885fa0e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-12 14:21:41 +0100

    bugfix in simple_fit_interactive, and added regression tests for the latest bugfix

--- simple_fit_interactive.py
+++ simple_fit_interactive.py
@@ -18,8 +18,8 @@ spec.plotter()
 # Subtract a baseline (the data is only 'mostly' reduced)
 spec.baseline(interactive=True)
 event1 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,143,223,button=1)
-event2 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,400,223,button=1)
-event3 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,472,223,button=1)
+event2 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,300,223,button=1)
+event3 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,572,223,button=1)
 event4 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,787,223,button=1)
 event5 = matplotlib.backend_bases.MouseEvent('button_press_event', spec.plotter.axis.figure.canvas,787,223,button=2)
 spec.baseline.event_manager(event1)
@@ -27,6 +27,7 @@ spec.baseline.event_manager(event2)
 spec.baseline.event_manager(event3)
 spec.baseline.event_manager(event4)
 spec.baseline.event_manager(event5)
+spec.baseline.highlight_fitregion()
 
 spec.specfit(interactive=True)
 event1 = matplotlib.backend_bases.KeyEvent('button_press_event', spec.plotter.axis.figure.canvas,x=463,y=570,key=2)
@@ -40,7 +41,9 @@ spec.specfit.event_manager(event3)
 spec.specfit.event_manager(event4)
 spec.specfit.event_manager(event5)
 
+print "Includemask before excludefit: ",spec.xarr[spec.baseline.includemask]," length = ",spec.baseline.includemask.sum()
 spec.baseline(excludefit=True)
+print "Includemask after excludefit: ",spec.xarr[spec.baseline.includemask]," length = ",spec.baseline.includemask.sum()
 spec.specfit(guesses=spec.specfit.modelpars)
 
 spec.plotter.figure.savefig(savedir+"simple_fit_interactive_HCOp.png")
@@ -63,11 +66,17 @@ spec.plotter.figure.canvas.toolbar.drag_zoom(event3)
 spec.plotter.figure.canvas.toolbar.release_zoom(event4)
 
 #spec.plotter.debug=True
+print "Includemask before excludefit with window limits: ",spec.xarr[spec.baseline.includemask]," length = ",spec.baseline.includemask.sum()
 spec.baseline(excludefit=True,use_window_limits=True,highlight=True)
+print "Includemask after excludefit with window limits: ",spec.xarr[spec.baseline.includemask]," length = ",spec.baseline.includemask.sum()
 spec.specfit(use_window_limits=True)
+spec.baseline.highlight_fitregion()
 
 # Regression test: make sure baseline selection works
-assert spec.baseline.includemask.sum() == 107
+# this should *NOT* be 107!  107 is ALL data between -100 and +20
+# this should be 102, which tells you that the fit has been excluded!
+# (note added 2/12/2014)
+assert spec.baseline.includemask.sum() == 102
 
 event1 = matplotlib.backend_bases.KeyEvent('key_press_event', spec.plotter.axis.figure.canvas,key='B')
 spec.plotter.parse_keys(event1)
@@ -89,3 +98,13 @@ assert spec.baseline.includemask.sum() == 264
 event2.inaxes,event2.button,event2.xdata,event2.ydata = spec.plotter.axis,3,244,0.1
 spec.baseline.event_manager(event2,debug=True)
 np.testing.assert_array_almost_equal(spec.baseline.baselinepars, np.array([-0.00016474, -0.01488391]))
+
+
+spec.baseline.selectregion(reset=True)
+assert np.all(spec.baseline.includemask)
+spec.baseline(interactive=True)
+assert np.all(spec.baseline.includemask)
+# reset_selection intentionally has a different behavior than reset
+# for interactive, you want POSITIVE selection, not negative
+spec.baseline(interactive=True, reset_selection=True)
+assert np.all(True-spec.baseline.includemask)
