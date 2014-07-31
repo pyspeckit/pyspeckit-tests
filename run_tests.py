e9b2aa49ff72b419f9ee199bc68e33175117abaf
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-31 10:28:54 +0200

    run_tests attempts to fix

--- run_tests.py
+++ run_tests.py
@@ -9,62 +9,71 @@ if interactive:
 else:
     ioff()
 
-#import pytest
-import pyspeckit.spectrum.tests.test_units
-for p in pyspeckit.spectrum.tests.test_units.params:
-    pyspeckit.spectrum.tests.test_units.test_convert_units(*p)
-    pyspeckit.spectrum.tests.test_units.test_convert_back(*p)
-#pytest.main()
+#def test_units():
+#    from pyspeckit.spectrum.tests import test_units as tu
+#    for p in tu.params:
+#        tu.test_convert_units(*p)
+#        tu.test_convert_back(*p)
 
-dir_prefix = os.path.split(__file__)[0]
+dir_prefix = os.path.split(os.path.abspath(__file__))[0]
 if os.path.exists(dir_prefix):
     os.chdir(dir_prefix)
 
-versnum = subprocess.Popen(["hg","id","--num"],stdout=subprocess.PIPE).communicate()[0].strip().strip("+")
-if versnum != "":
-    savedir = "tests_%s/" % versnum
-    if not os.path.exists(savedir):
-        os.mkdir(savedir)
-else:
-    savedir = ""
+savedir = ''
 
+if False:
+    versnum = subprocess.Popen(["hg","id","--num"],stdout=subprocess.PIPE).communicate()[0].strip().strip("+")
+    if versnum != "":
+        savedir = "tests_%s/" % versnum
+        if not os.path.exists(savedir):
+            os.mkdir(savedir)
+    else:
+        savedir = ""
 
-print "*****test_fits.py*****"
-execfile('test_fits.py',{'interactive':interactive,'savedir':savedir})
-print "*****test_hr2421.py*****"
-execfile('test_hr2421.py',{'interactive':interactive,'savedir':savedir})
-#print "*****test_nh3.py*****"
-#execfile('test_nh3.py',{'interactive':interactive,'savedir':savedir})
-print "*****test_sdss.py*****"
-execfile('test_sdss.py',{'interactive':interactive,'savedir':savedir})
-print "*****test_txt.py*****"
-execfile('test_txt.py',{'interactive':interactive,'savedir':savedir})
-print "*****simple_fit_example.py*****"
-execfile('simple_fit_example.py',{'interactive':interactive,'savedir':savedir})
-print "*****simple_fit_interactive.py*****"
-execfile('simple_fit_interactive.py',{'interactive':interactive,'savedir':savedir})
-print "*****alberto_example.py*****"
-execfile('alberto_example.py',{'interactive':interactive,'savedir':savedir})
+def test_everything():
 
-print "*****test_formaldehyde_radex.py*****"
-execfile('test_formaldehyde_radex.py',{'interactive':interactive,'savedir':savedir})
-print "*****test_formaldehyde.py*****"
-execfile('test_formaldehyde.py',{'interactive':interactive,'savedir':savedir})
+    #test_units()
+    from pyspeckit.spectrum.tests import test_units as tu
+    for p in tu.params:
+        tu.test_convert_units(*p)
+        tu.test_convert_back(*p)
 
-print "*****vega_echelle.py*****"
-execfile('vega_echelle_example.py',{'interactive':interactive,'savedir':savedir})
+    print "*****test_fits.py*****"
+    execfile('test_fits.py',{'interactive':interactive,'savedir':savedir})
+    print "*****test_hr2421.py*****"
+    execfile('test_hr2421.py',{'interactive':interactive,'savedir':savedir})
+    #print "*****test_nh3.py*****"
+    #execfile('test_nh3.py',{'interactive':interactive,'savedir':savedir})
+    print "*****test_sdss.py*****"
+    execfile('test_sdss.py',{'interactive':interactive,'savedir':savedir})
+    print "*****test_txt.py*****"
+    execfile('test_txt.py',{'interactive':interactive,'savedir':savedir})
+    print "*****simple_fit_example.py*****"
+    execfile('simple_fit_example.py',{'interactive':interactive,'savedir':savedir})
+    print "*****simple_fit_interactive.py*****"
+    execfile('simple_fit_interactive.py',{'interactive':interactive,'savedir':savedir})
+    print "*****alberto_example.py*****"
+    execfile('alberto_example.py',{'interactive':interactive,'savedir':savedir})
 
-print "*****test_voigt.py*****"
-execfile('test_voigt.py')
+    print "*****test_formaldehyde_radex.py*****"
+    execfile('test_formaldehyde_radex.py',{'interactive':interactive,'savedir':savedir})
+    print "*****test_formaldehyde.py*****"
+    execfile('test_formaldehyde.py',{'interactive':interactive,'savedir':savedir})
 
-print "*****test_juliantxt.py*****"
-execfile('test_juliantxt.py')
+    print "*****vega_echelle.py*****"
+    execfile('vega_echelle_example.py',{'interactive':interactive,'savedir':savedir})
 
-print "Success!  Or at least, no exceptions..."
+    print "*****test_voigt.py*****"
+    execfile('test_voigt.py')
 
-try:
-    print "Running comparison"
-    execfile('compare_images.py')
-except ImportError:
-    print "Not comparing images because PIL was not installed."
+    print "*****test_juliantxt.py*****"
+    execfile('test_juliantxt.py')
+
+    print "Success!  Or at least, no exceptions..."
+
+    #try:
+    #    print "Running comparison"
+    #    execfile('compare_images.py')
+    #except ImportError:
+    #    print "Not comparing images because PIL was not installed."
 
