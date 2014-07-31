b3250754a2d303227125e8ca343e7d8f6a1487d3
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-31 12:39:44 +0200

    execute appropriate files in inappropriate directories

--- run_tests.py
+++ run_tests.py
@@ -18,42 +18,47 @@ def test_everything(savedir=''):
         tu.test_convert_units(*p)
         tu.test_convert_back(*p)
 
+    curpath = os.getcwd()
+    dir_prefix = os.path.split(os.path.abspath(__file__))[0]
+    os.chdir(dir_prefix)
+
     print "*****test_fits.py*****"
-    execfile('test_fits.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'test_fits.py'),{'interactive':interactive,'savedir':savedir})
     print "*****test_hr2421.py*****"
-    execfile('test_hr2421.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'test_hr2421.py'),{'interactive':interactive,'savedir':savedir})
     #print "*****test_nh3.py*****"
-    #execfile('test_nh3.py',{'interactive':interactive,'savedir':savedir})
+    #execfile(os.path.join(dir_prefix,'test_nh3.py'),{'interactive':interactive,'savedir':savedir})
     print "*****test_sdss.py*****"
-    execfile('test_sdss.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'test_sdss.py'),{'interactive':interactive,'savedir':savedir})
     print "*****test_txt.py*****"
-    execfile('test_txt.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'test_txt.py'),{'interactive':interactive,'savedir':savedir})
     print "*****simple_fit_example.py*****"
-    execfile('simple_fit_example.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'simple_fit_example.py'),{'interactive':interactive,'savedir':savedir})
     print "*****simple_fit_interactive.py*****"
-    execfile('simple_fit_interactive.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'simple_fit_interactive.py'),{'interactive':interactive,'savedir':savedir})
     print "*****alberto_example.py*****"
-    execfile('alberto_example.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'alberto_example.py'),{'interactive':interactive,'savedir':savedir})
 
     print "*****test_formaldehyde_radex.py*****"
-    execfile('test_formaldehyde_radex.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'test_formaldehyde_radex.py'),{'interactive':interactive,'savedir':savedir})
     print "*****test_formaldehyde.py*****"
-    execfile('test_formaldehyde.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'test_formaldehyde.py'),{'interactive':interactive,'savedir':savedir})
 
     print "*****vega_echelle.py*****"
-    execfile('vega_echelle_example.py',{'interactive':interactive,'savedir':savedir})
+    execfile(os.path.join(dir_prefix,'vega_echelle_example.py'),{'interactive':interactive,'savedir':savedir})
 
     print "*****test_voigt.py*****"
-    execfile('test_voigt.py')
+    execfile(os.path.join(dir_prefix,'test_voigt.py'))
 
     print "*****test_juliantxt.py*****"
-    execfile('test_juliantxt.py')
+    execfile(os.path.join(dir_prefix,'test_juliantxt.py'))
 
     print "Success!  Or at least, no exceptions..."
+    os.chdir(curpath)
 
     #try:
     #    print "Running comparison"
-    #    execfile('compare_images.py')
+    #    execfile(os.path.join(dir_prefix,'compare_images.py'))
     #except ImportError:
     #    print "Not comparing images because PIL was not installed."
 
