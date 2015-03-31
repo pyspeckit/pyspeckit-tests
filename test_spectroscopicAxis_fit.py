ab6d0002be4c6de909920498980595cafec46118
Dinos Kousidis <dinos.kousidis@tum.de>
2015-03-31 09:54:42 +0200

    added noise to the data

--- test_spectroscopicAxis_fit.py
+++ test_spectroscopicAxis_fit.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pyspeckit.spectrum.models.gaussfitter import gaussian_fitter
+from pyspeckit.spectrum.models.inherited_gaussfitter import gaussian_fitter
 from pyspeckit import Spectrum
 
 xarr = np.linspace(-100,100,200)
@@ -8,14 +8,14 @@ params = [1.0, 2.0, 0.5]
 passes = {'passes': [0,0,0], 'avg_difference': [0,0,0]}
 fails = {'fails': [0,0,0], 'avg_difference': [0, 0, 0]}
 
-for i in range(100):
+for i in range(1):
 	parameter_error_amplitude = [1/5., 5., 1.]
-	parameter_noise = [params[0]*+ abs(np.random.randn()) * parameter_error_amplitude[0], 
+	parameter_noise = [params[0] + abs(np.random.randn()) * parameter_error_amplitude[0], 
                        params[1] + abs(np.random.randn()) * parameter_error_amplitude[1],
                        params[2] + abs(np.random.randn()) * parameter_error_amplitude[2]]
 	guesses = np.array(params)+parameter_noise
 
-	sp = Spectrum(xarr=xarr, data=gf.n_modelfunc(pars=params)(xarr))
+	sp = Spectrum(xarr=xarr, data=gf.n_modelfunc(pars=params)(xarr)+np.random.randn(xarr.size)/100., error=np.ones(xarr.size)/100.)
 	sp.specfit(guesses=guesses, verbose=False)
 	print 'params:',params
 	print 'guesses:',guesses
@@ -34,6 +34,7 @@ for i,j in enumerate(passes['avg_difference']):
 		passes['avg_difference'][i] = j/passes['passes'][i]
 for i,j in enumerate(fails['avg_difference']):
 	if fails['fails'][i]:
+		pass
 
 print 'passes:', passes
 print 'fails:', fails
