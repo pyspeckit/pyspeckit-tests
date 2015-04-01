edd531c2ec31f808b55d19679206a94968e603a9
Dinos Kousidis <dinos.kousidis@tum.de>
2015-04-01 15:25:46 +0200

    last state before parametrization

--- test_Cube_fiteach.py
+++ test_Cube_fiteach.py
@@ -3,34 +3,37 @@ from pyspeckit import SpectralCube
 from pyspeckit.spectrum.units import SpectroscopicAxis
 from pyspeckit.spectrum.models.inherited_gaussfitter import gaussian_fitter
 from astropy import units as u
+import matplotlib.pyplot as plt
 
-xarr = SpectroscopicAxis(np.linspace(-100, 100, 200), unit=u.Hz)
+xarr = SpectroscopicAxis(np.linspace(-100, 100, 200), unit=u.dimensionless_unscaled)
 gf = gaussian_fitter()
-params = [1., 2., 5.]
+params = [1.0, 2.0, 5.0]
 parameter_error_amplitude = [5., 5., 5.]
-assertion = []
+assertion_list = []
 passes = {'passes': [0,0,0], 'avg_difference': [0,0,0]}
 fails = {'fails': [0,0,0], 'avg_difference': [0, 0, 0]}
 
-for i in range(20):
+for i in range(10):
+	passed = True
 	parameter_noise = [params[0] + abs(np.random.randn()) * parameter_error_amplitude[0], 
 	                   params[1] + abs(np.random.randn()) * parameter_error_amplitude[1],
 	                   params[2] + abs(np.random.randn()) * parameter_error_amplitude[2]]
 	guesses = np.array(params)+parameter_noise
 	errspec = np.ones(xarr.size)
-	data = gf.n_modelfunc(pars=params)(xarr)*100.+np.random.randn(xarr.size)
-
+	data = gf.n_modelfunc(pars=params)(xarr.value)+np.random.randn(xarr.value.size)/100.
 	cube = [[[x for x in data] for y in range(2)] for z in range(2)]
 	cube = np.array(cube)
 	cube = np.rollaxis(cube, 2, 0)
 	sp = SpectralCube.Cube(xarr=xarr, cube=cube, unit='Hz')
-	sp.fiteach(guesses=guesses, errspec=errspec, fitkwargs={'guesses':guesses})
+	sp.fiteach(guesses=guesses, errspec=errspec, signal_cut=0, fitkwargs={'guesses':guesses})
+	# sp.plotter(axis=plt.gca())
+	# plt.show()
 	result_cube = sp.parcube
+	print 'sp.parcube:',sp.parcube
 	for i in range(2):
 		for j in range(2):
-			print result_cube[:,i,j]
-			passed = True
-			for k,result in enumerate(result_cube[:,i,j]):
+			assertion = ((np.array(result_cube[:,i,j])-np.array(params))/parameter_noise)**2
+			for k,result in enumerate(assertion):
 				if result >= 2: 
 					passed = False
 					continue
@@ -42,9 +45,8 @@ for i in range(20):
 				for k in range(3):
 					fails['fails'][k]+=1
 					fails['avg_difference'][k] += abs(result_cube[:,i,j][k] - params[k])
-			assertion.append(((np.array(result_cube[:,i,j])-np.array(params))/parameter_noise)**2)
+			assertion_list.append(assertion)
 
-print 'result:', result
-print 'assertion:', np.array(assertion)
+print 'assertion_list:', np.array(assertion_list)
 print 'passes:', passes
 print 'fails:', fails
--- test_spectroscopicAxis_fit.py
+++ test_spectroscopicAxis_fit.py
@@ -6,24 +6,21 @@ import matplotlib.pyplot as plt
 xarr = np.linspace(-100,100,200)
 gf = gaussian_fitter()
 params = [1.0, 2.0, 20.]
+parameter_error_amplitude = [5., 5., 5.]
 passes = {'passes': [0,0,0], 'avg_difference': [0,0,0]}
 fails = {'fails': [0,0,0], 'avg_difference': [0, 0, 0]}
 
 for i in range(100):
 	passed = True
-	parameter_error_amplitude = [1/5, 1., 5.]
 	parameter_noise = [params[0] + abs(np.random.randn()) * parameter_error_amplitude[0], 
                        params[1] + abs(np.random.randn()) * parameter_error_amplitude[1],
                        params[2] + abs(np.random.randn()) * parameter_error_amplitude[2]]
 	guesses = np.array(params)+parameter_noise
 	sp = Spectrum(xarr=xarr, data=gf.n_modelfunc(pars=params)(xarr)+np.random.randn(xarr.size)/100., error=np.ones(xarr.size)/100)
-	# sp = Spectrum(xarr=xarr, data=gf.n_modelfunc(pars=params)(xarr))
-	sp.plotter(axis=plt.gca())
+	# sp.plotter(axis=plt.gca(), errstyle='fill')
 	sp.specfit(guesses=guesses)
-	print 'guesses:',guesses
 	assertion = ((np.array(sp.specfit.fitter.mpp)-np.array(params))/parameter_noise)**2
 	print '(mpp - params / param_noise )^2 = ', assertion
-	
 	for j,result in enumerate(assertion):
 		if result >= 2: 
 			passed = False
@@ -31,11 +28,11 @@ for i in range(100):
 	if passed:
 		for j in range(3):
 			passes['passes'][j]+=1
-			passes['avg_difference'][j] += abs(assertion[j] - params[j])
+			passes['avg_difference'][j] += abs(sp.specfit.fitter.mpp[j] - params[j])
 	else:
 		for j in range(3):
 			fails['fails'][j]+=1
-			fails['avg_difference'][j] += abs(assertion[j] - params[j])
+			fails['avg_difference'][j] += abs(sp.specfit.fitter.mpp[j] - params[j])
 	# plt.show()
 
 for i,j in enumerate(passes['avg_difference']):
