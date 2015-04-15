612f198fa98ab006f307e931acb183905f431a1f
Dinos Kousidis <dinos.kousidis@tum.de>
2015-04-15 17:16:48 +0200

    added documentation and added test_moments.py to run_tests.py

--- test_moments.py
+++ test_moments.py
@@ -1,3 +1,8 @@
+########################################################################
+# This test checks if the moments() method for every model             #
+# in Registry.multifitters produces the correct number of paramerters. #
+########################################################################
+
 from pyspeckit.spectrum.units import SpectroscopicAxis
 from pyspeckit import Spectrum
 import numpy as np
@@ -5,16 +10,20 @@ import pytest
 
 xarr = np.linspace(-100,100,200)
 
+# creating a sample Spectrum from a .fits file
 sp = Spectrum('test.fits')
 for model_name in sp.specfit.Registry.multifitters.iterkeys():
     try:
         print 'testing:', model_name
         model = sp.specfit.Registry.multifitters[model_name]
         moments = getattr(model, 'moments')
+        # creating the random data which are not relevant
         rawdata = np.random.randn(xarr.size)
         params = model.moments(xarr, rawdata)
         print 'params from moments:',params
         xarr = SpectroscopicAxis(xarr)
+        # if this call does not raise an Exception then moments() produced
+        # the correct number of parameters 
         model.n_modelfunc(pars=params)(xarr)
     except Exception as e:
         pytest.fail(e)
