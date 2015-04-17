f71ac57bf0ea95c8b8c0ebc887ae95d702644ff5
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-17 10:27:13 +0200

    units to unit

--- alberto_example.py
+++ alberto_example.py
@@ -24,8 +24,8 @@ for i in range(len(files)):
     flux = X[i][:,0]**2/c*1e29*X[i][:,1] # microJy
     fluxerr = X[i][:,0]**2/c*1e29*X[i][:,2] # microJy
 
-    spec.append(pyspeckit.Spectrum(xarr=wav,data=flux,err=fluxerr,units='Jy',xarrkwargs={'unit':'angstrom'}))
-    spec[i].units = 'Jy'
+    spec.append(pyspeckit.Spectrum(xarr=wav,data=flux,err=fluxerr,unit='Jy',xarrkwargs={'unit':'angstrom'}))
+    spec[i].unit = 'Jy'
     spec[i].xarr.frame = 'obs'
     spec[i].xarr.redshift = z0
     #my_fitter = f.SpectralModel(hill5_model,3,parnames=['amplitude','wav','width'],parlimited=[(False,False),(False,False),(False,False)],parlimits=[(0,0),(0,0),(0,0),(0,0),(0,0)],shortvarnames=('A','\lambda','\sigma'))
