582ce56ad571ff91a40d29ff7553910ea5b60410
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-26 08:21:11 +0200

    fix moment tests

--- test_moments.py
+++ test_moments.py
@@ -7,8 +7,12 @@ from pyspeckit.spectrum.units import SpectroscopicAxis
 from pyspeckit import Spectrum
 import numpy as np
 import pytest
+from astropy import units as u
 
-xarr = np.linspace(-100,100,200)
+xvals = np.linspace(-100,100,200) * u.km/u.s
+# Some models will fail without a center frequency and a velocity convention
+xarr = SpectroscopicAxis(xvals, velocity_convention='radio', center_frequency=100*u.GHz)
+rawdata = np.random.randn(xarr.size)
 
 # creating a sample Spectrum from a .fits file
 sp = Spectrum('test.fits')
@@ -18,10 +22,8 @@ for model_name in sp.specfit.Registry.multifitters.iterkeys():
         model = sp.specfit.Registry.multifitters[model_name]
         moments = getattr(model, 'moments')
         # creating the random data which are not relevant
-        rawdata = np.random.randn(xarr.size)
         params = model.moments(xarr, rawdata)
         print 'params from moments:',params
-        xarr = SpectroscopicAxis(xarr)
         # if this call does not raise an Exception then moments() produced
         # the correct number of parameters 
         model.n_modelfunc(pars=params)(xarr)
