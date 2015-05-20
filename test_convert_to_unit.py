a0612f61e73a7875dde2ca18cf8a0e0dc43d7de2
Dinos Kousidis <dinos.kousidis@tum.de>
2015-05-20 12:31:13 +0200

    additions to test_convert_to_unit.py

--- test_convert_to_unit.py
+++ test_convert_to_unit.py
@@ -10,24 +10,26 @@ import numpy as np
 import inspect
 import pytest
 
-def test_convert_to_unit():
-	run_with_assert = False
-	for from_unit, to_unit, _, __ in u.doppler_optical(1*u.Hz):
-		sp = SpectroscopicAxis(np.linspace(-100,100,100), unit=from_unit, refX=23.2*u.Hz, velocity_convention='optical')
-		sp.convert_to_unit(to_unit)
-		print sp
-		if(run_with_assert):
-			assert sp.unit == to_unit
-	for from_unit, to_unit, _, __ in u.doppler_radio(1*u.eV):
-		sp = SpectroscopicAxis(np.linspace(-100,100,100), unit=from_unit, refX=23.2*u.Hz, velocity_convention='optical')
-		sp.convert_to_unit(to_unit)
-		print sp
-		if(run_with_assert):
-			assert sp.unit == to_unit
-	for from_unit, to_unit, _, __ in u.doppler_relativistic(1*u.Angstrom):
-		sp = SpectroscopicAxis(np.linspace(-100,100,100), unit=from_unit, refX=23.2*u.Hz, velocity_convention='relativistic')
-		sp.convert_to_unit(to_unit)
-		print sp
-		if(run_with_assert):
-			assert sp.unit == to_unit
-		
+def test_convert_to_unit(run_with_assert=False):
+    for from_unit, to_unit, _, __ in u.doppler_optical(1*u.Hz):
+        sp = SpectroscopicAxis(np.linspace(-100,100,100),
+                               unit=from_unit, refX=23.2*u.Hz,
+                               velocity_convention='optical')
+        sp.convert_to_unit(to_unit)
+        if(run_with_assert):
+            assert sp.unit == to_unit
+    for from_unit, to_unit, _, __ in u.doppler_radio(1*u.eV):
+        sp = SpectroscopicAxis(np.linspace(-100,100,100), 
+                               unit=from_unit, refX=23.2*u.Hz,
+                               velocity_convention='optical')
+        sp.convert_to_unit(to_unit)
+        if(run_with_assert):
+            assert sp.unit == to_unit
+    for from_unit, to_unit, _, __ in u.doppler_relativistic(1*u.Angstrom):
+        sp = SpectroscopicAxis(np.linspace(-100,100,100), 
+                               unit=from_unit, refX=23.2*u.Hz,
+                               velocity_convention='relativistic')
+        sp.convert_to_unit(to_unit)
+        if(run_with_assert):
+            assert sp.unit == to_unit
+        
