0f86e6938e386dee20716af617f79244357faa1a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-08-10 12:00:20 +0200

    add some more basic tests for cubes

--- test_cube_init.py
+++ test_cube_init.py
@@ -1,16 +1,21 @@
 from __future__ import print_function
 import numpy as np
-from astropy.io import fits
 import pyspeckit
-import matplotlib.pyplot as plt
+from astropy import units as u
 
-mycube= np.random.randn(250,50,50)
-myaxis= np.linspace(-100,100,250)
-pcube=pyspeckit.Cube(cube=mycube, xarr=myaxis, xunit='km/s')
+mycube = np.random.randn(250,50,50)
+myaxis = np.linspace(-100,100,250)
+pcube=pyspeckit.Cube(cube=mycube, xarr=myaxis, xunit='km/s',
+                     xarrkwargs=dict(refX=1*u.GHz, velocity_convention='radio'))
+pcube.xarr.velocity_convention='radio'
+pcube.xarr.refX=1*u.GHz
 
-assert pcube.xarr.flags['OWNDATA']
+pcube.xarr.convert_to_unit('m/s')
 
 sp = pcube.get_spectrum(5,5)
 
 print(pcube)
 print(pcube.__repr__())
+
+stack = pyspeckit.CubeStack([pcube, pcube])
+stack.xarr.convert_to_unit(u.km/u.s)
