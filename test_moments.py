ab71ea5d20161fa8dee6f263df5fdbc5ec647386
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-10-12 15:32:38 +0200

    I don't know why there was a try/pytest.fail combination here

--- test_moments.py
+++ test_moments.py
@@ -17,15 +17,12 @@ rawdata = np.random.randn(xarr.size)
 # creating a sample Spectrum from a .fits file
 sp = Spectrum('test.fits')
 for model_name in sp.specfit.Registry.multifitters.iterkeys():
-    try:
-        print 'testing:', model_name
-        model = sp.specfit.Registry.multifitters[model_name]
-        moments = getattr(model, 'moments')
-        # creating the random data which are not relevant
-        params = model.moments(xarr, rawdata)
-        print 'params from moments:',params
-        # if this call does not raise an Exception then moments() produced
-        # the correct number of parameters 
-        model.n_modelfunc(pars=params)(xarr)
-    except Exception as e:
-        pytest.fail(e)
+    print 'testing:', model_name
+    model = sp.specfit.Registry.multifitters[model_name]
+    moments = getattr(model, 'moments')
+    # creating the random data which are not relevant
+    params = model.moments(xarr, rawdata)
+    print 'params from moments:',params
+    # if this call does not raise an Exception then moments() produced
+    # the correct number of parameters 
+    model.n_modelfunc(pars=params)(xarr)
