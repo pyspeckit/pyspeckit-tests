f62aa51284716e53f8cea12d3e168fcf2d1a77b3
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-26 18:57:45 +0100

    skip voigt test

--- test_voigt.py
+++ test_voigt.py
@@ -1,12 +1,21 @@
-import numpy as np
-import pyspeckit.spectrum.models.inherited_voigtfitter as IV
-from pyspeckit.spectrum.units import SpectroscopicAxis
+try:
+    import scipy
+    scipyOK = True
+except ImportError:
+    scipyOK = False
 
-xarr = SpectroscopicAxis(np.linspace(-100,100,1000))
-dx = np.diff(xarr).mean()
+if scipyOK:
+    import numpy as np
+    import pyspeckit.spectrum.models.inherited_voigtfitter as IV
+    from pyspeckit.spectrum.units import SpectroscopicAxis
 
-V1 = IV.voigt(xarr,1,0,1,1,normalized=False)
-V2 = IV.voigt(xarr,1,0,1,1,normalized=True)
+    xarr = SpectroscopicAxis(np.linspace(-100,100,1000))
+    dx = np.diff(xarr).mean()
 
-assert np.sqrt(2*np.pi) - 0.05 < V1.sum()*dx.value < np.sqrt(2*np.pi) + 0.05 
-assert 0.99 < V2.sum()*dx.value < 1.01
+    V1 = IV.voigt(xarr,1,0,1,1,normalized=False)
+    V2 = IV.voigt(xarr,1,0,1,1,normalized=True)
+
+    assert np.sqrt(2*np.pi) - 0.05 < V1.sum()*dx.value < np.sqrt(2*np.pi) + 0.05 
+    assert 0.99 < V2.sum()*dx.value < 1.01
+else:
+    print("Skipped Voigt test because scipy could not be imported")
