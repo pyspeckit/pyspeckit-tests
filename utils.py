fa7eed24615b4e60aa77dc7d5a0c3ec76958d09b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-09 09:31:39 +0200

    make tests more verbose

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
-def run_tests(module):
+from astropy import log
+
+def run_tests(module, verbose=True):
 
     for entry in dir(module):
         if entry[:4] == 'test':
             testfunc = getattr(module, entry)
+            if verbose:
+                log.info("Running test {0}".format(entry))
             return testfunc()
