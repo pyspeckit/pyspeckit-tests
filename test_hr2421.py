84b9042f1758674a45798e043ec6881079e4a6c4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-11-28 12:53:04 -0700

    fix a lot of broken things in the hr2421 test (there were some incorrect guesses caused by the size of the image and some bad things related to baselines - there are a lot of hidden errors here and this script could be turned into its own whole test suite...)

--- test_hr2421.py
+++ test_hr2421.py
@@ -4,6 +4,7 @@ import pyspeckit
 import matplotlib
 import warnings
 from astropy.io import fits
+from astropy import units as u
 
 if not 'interactive' in globals():
     interactive=False
@@ -39,10 +40,12 @@ print("Plotter min/max: ",sp.plotter.xmin,sp.plotter.xmax," Fitter min/max: ",sp
 if interactive: raw_input("Wait here a moment")
 import numpy as np;
 print(np.geterr())
-sp.baseline(subtract=False,exclude=[4830,4890],order=2,highlight=True)
+sp.baseline(subtract=False, exclude=[4830, 4890], order=2, highlight=True)
+assert sp.baseline.basespec[sp.xarr.x_to_pix(4860)] > 0
 print("Plotter min/max: ",sp.plotter.xmin,sp.plotter.xmax," Fitter min/max: ",sp.specfit.xmin,sp.specfit.xmax," Fitregion= ",sp.baseline.button1plot," bfit target sum: ",sp.baseline.includemask.sum())
 # obsolete 1/16/2012 print("Baseline exclude: ",sp.baseline.excludevelo,sp.baseline.excludepix)
-if interactive: raw_input("Wait here a moment")
+if interactive:
+    raw_input("Wait here a moment")
 
 # set the baseline to zero to prevent variable-height fitting
 # (if you don't do this, the best fit to the spectrum is dominated by the
@@ -75,22 +78,43 @@ try:
     if savedir != "":
         sp.plotter.figure.savefig(savedir+'hr2421_voigtfit.png')
     voigt_model = sp.specfit.model+sp.baseline.basespec[sp.specfit.xmin:sp.specfit.xmax]
+    print("Voigt baseline: {0}".format(sp.baseline.baselinepars))
     print("A voigt model has been fit.  The red line from before should have a blue line overlaid.  They should be only moderately different.")
-    if interactive: raw_input("Wait here a moment")
+    if interactive:
+        raw_input("Wait here a moment")
 except ImportError:
     print("Could not fit voigt profiles because scipy wasn't installed.")
 
+print()
+
+# refit the baseline
+sp.baseline(subtract=False, exclude=[4830, 4890], order=2, highlight=True)
+
+# convert from data locations to plotter locations
+x1d = 4800
+x2d = 4900
+x3d = 4860
+y3d = sp.slice(4800*u.AA, 4900*u.AA).data.min()
+x4d = 4850
+y4d = sp.data[np.int(sp.xarr.x_to_pix(4879))]
+x5d = 4880
+y1d = sp.slice(4800*u.AA, 4900*u.AA).data.mean()
+x1,y1 = sp.plotter.axis.transData.transform([x1d,y1d])
+x2,y1 = sp.plotter.axis.transData.transform([x2d,y1d])
+x3,y3 = sp.plotter.axis.transData.transform([x3d,y3d])
+x4,y4 = sp.plotter.axis.transData.transform([x4d,y4d])
+x5,y1 = sp.plotter.axis.transData.transform([x5d,y1d])
 
 sp.specfit(interactive=True)
 print("INTERACTIVE #1: fittype=",sp.specfit.fittype," npars: ",sp.specfit.fitter.npars)
 if interactive:
     raw_input('Press enter to printguesses and best fit and end code')
 else:
-    event1 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,257,316,button=1)
-    event2 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,732,280,button=1)
-    event3 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,523,194,button=2)
-    event4 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,485,264,button=2)
-    event5 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,611,247,button=3)
+    event1 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x1,y1,button=1)
+    event2 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x2,y1,button=1)
+    event3 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x3,y3,button=2)
+    event4 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x4,y4,button=2)
+    event5 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x5,y1,button=3)
     sp.specfit.event_manager(event1)
     sp.specfit.event_manager(event2)
     if savedir != "":
@@ -109,6 +133,9 @@ print("Best fit: ", sp.specfit.modelpars)
 print("EQW: ",sp.specfit.EQW(fitted=False))
 print("EQW (fitted): ",sp.specfit.EQW(fitted=True))
 
+# double check that the baseline is still OK
+assert sp.baseline.basespec[sp.xarr.x_to_pix(4860)] > 0
+
 sp.plotter(xmin=4700,xmax=5000)
 eventF = matplotlib.backend_bases.KeyEvent('key_press_event', sp.plotter.axis.figure.canvas,key='f',x=257,y=316)
 eventV = matplotlib.backend_bases.KeyEvent('key_press_event', sp.plotter.axis.figure.canvas,key='v',x=257,y=316)
@@ -116,17 +143,31 @@ sp.specfit.event_manager(eventF)
 sp.specfit.event_manager(eventV)
 print("INTERACTIVE #2: fittype=",sp.specfit.fittype," npars: ",sp.specfit.fitter.npars)
 
-event1 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,257,316,button=1)
-event2 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,732,280,button=1)
-event3 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,523,194,button=2)
-event4 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,485,264,button=2)
-event5 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,483,262,button=2)
-event6 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,611,247,button=3)
+x1,y1 = sp.plotter.axis.transData.transform([x1d,y1d])
+x2,y1 = sp.plotter.axis.transData.transform([x2d,y1d])
+x3,y3 = sp.plotter.axis.transData.transform([x3d,y3d])
+x4,y4 = sp.plotter.axis.transData.transform([x4d,y4d])
+x5,y1 = sp.plotter.axis.transData.transform([x5d,y1d])
+
+# double check that the baseline is still OK
+assert sp.baseline.basespec[sp.xarr.x_to_pix(4860)] > 0
+
+event1 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x1,y1,button=1)
+event2 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x2,y1,button=1)
+event3 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x3,y3,button=2)
+event4 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x4,y4,button=2)
+event5 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x5,y1,button=2)
+event6 = matplotlib.backend_bases.MouseEvent('button_press_event', sp.plotter.axis.figure.canvas,x4,y1,button=3)
 sp.specfit.event_manager(event1,debug=True)
 sp.specfit.event_manager(event2,debug=True)
 sp.specfit.event_manager(event3,debug=True)
 sp.specfit.event_manager(event4,debug=True)
 sp.specfit.event_manager(event5,debug=True)
+
+# since this is an absorption line, the guess should be negative
+# (guesses are only set after the 3rd (nwidths) click)
+assert sp.specfit.guesses[0] < 0
+
 if savedir != "":
     sp.plotter.figure.savefig(savedir+'hr2421_interactive_guesses_oneextra.png')
 sp.specfit.event_manager(event6,debug=True)
