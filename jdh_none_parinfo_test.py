10c8a3f41bf40ee8714d35afd4a1a287f1e43ce4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-11-26 16:06:46 -0500

    fix tests; apparently .dat files aren't copied

--- jdh_none_parinfo_test.py
+++ jdh_none_parinfo_test.py
@@ -1,11 +1,14 @@
-import numpy as np
 from astropy.table import Table
 from astropy import log
 from pyspeckit.spectrum import Spectrum
 
-tbl = Table.read('problemspec.dat', format='ascii')
+import os
+print("current path: ",os.getcwd())
+print(os.listdir())
 
-sp = Spectrum(xarr=tbl['x'], data=tbl['y'].data)
+tbl = Table.read('problemspec.txt', format='ascii')
+
+sp = Spectrum(xarr=tbl['x'], data=tbl['y'].data, header={})
 
 #sp.specfit(guesses=[0.042577108420564426, 47.709708298124426, 0.6840788813117882], use_lmfit=True, quiet=False, debug=True)
 #print(sp.specfit.modelpars, sp.specfit.modelerrs)
